639dcd63779508b8a42373293e93d38806989b33
Troy Havelock <troy.havelock@snyk.io>
2023-06-07 09:50:33 -0400

    Adding k8s

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
+
 import (
 	"fmt"
+	"go-goof/handlers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gogs/gogs/pkg/tool"
 	"github.com/pstember/go-goof/hello"
-    "go-goof/handlers"
-    "github.com/gogs/gogs/pkg/tool"
-    "github.com/gin-gonic/gin"
 )
+
 func main() {
-		fmt.Println(tool.MD5(hello.Hello()))
-        r := gin.Default()
-        r.GET("/ping", handlers.Ping)
-        r.Run() // listen and serve on 0.0.0.0:8080
+	fmt.Println(tool.MD5(hello.Hello()))
+	r := gin.Default()
+	r.GET("/ping", handlers.Ping)
+	r.Run("0.0.0.0:8082") // listen and serve on 0.0.0.0:8080
 }
